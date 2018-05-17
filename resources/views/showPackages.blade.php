daccbbf5fe116d7cc7da26bdef83625abf9d8a61
Monster Truck <Hope777@users.noreply.github.com>
2018-05-17 02:38:48 +0100

    Update showPackages.blade.php

--- resources/views/showPackages.blade.php
+++ resources/views/showPackages.blade.php
@@ -9,11 +9,11 @@
     <div class="col-md-4 center-block" style="float:none;">
     <div class="panel panel-default">
       <div class="panel-heading">
-    <p><b>Package: </b><a href="booking/calendar/{{ $package->id }}">{{ $package->package_name }}</a><br>
+    <p><b>Room: </b><a href="booking/calendar/{{ $package->id }}">{{ $package->package_name }}</a><br>
       </div>
       <div class="panel-body">
-    <b>Time: </b>{{ $package->package_time }} hours<br>
-    <b>Price: </b>{{ $package->package_price }}<br>
+    <b>Area: </b>{{ $package->package_time }} <br>
+    <b>Number of Computers: </b>{{ $package->package_price }}<br>
     <b>Description: </b>{{ $package->package_description }}</p>
     </div>
 </div>
